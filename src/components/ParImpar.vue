7e9145e2f07e0243ff312b2aaae65698d5993c71
mmtol <mmigueltolosana@gmail.com>
2025-10-27 09:35:30 +0100

    par impar

--- src/components/ParImpar.vue
+++ src/components/ParImpar.vue
@@ -2,13 +2,30 @@
     <div>
         <h1>Par/Impar</h1>
         <label>Introduzca un numero</label>
+        <input type="number" v-model="numero"/>
+        <button @click="generarRandom">Generar número</button>
+        <h2 v-if="numero % 2 == 0">PAR</h2>
+        <h2 v-else>IMPAR</h2>
     </div>
 </template>
 
 <script>
 export default 
 {
-    
+    name:"ParImpar",
+    data()
+    {
+        return{
+            numero:0
+        }
+    },
+    methods:
+    {
+        generarRandom()
+        {
+            this.numero = parseInt(Math.random() * 50) + 1;
+        }
+    }
 }
 </script>
 
